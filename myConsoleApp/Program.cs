ca2ed03455eacefa72a056737d808d02707d3d62
YeabNigusse <nigusseyeabsira@gmail.com>
2023-12-20 10:19:56 +0300

    learned how to setup c#

---
+++ myConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+/*
+
+to create a dotnet project follow the folloowing steps
+
+install the SDK and check wether it is installed or net
+USE DOTNET NEW --LIST TO SEE ABVILABLE TEMPLETS
+USE DOTNET NEW CONSOLE -N <project name>
+use dotnet build to build the project
+use dotnet run to run the project
+
+*/
