472dbdf15f07389fb0ba9c4d2bb58bd90492481a
YeabNigusse <nigusseyeabsira@gmail.com>
2024-01-13 20:48:20 +0300

    excersising in c#

---
+++ yeab/Even_or_odd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace yeab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Number");
+            string input = Console.ReadLine();
+
+            // Parse the input string to an integer
+            if (int.TryParse(input, out int number))
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number + " is Even");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is odd");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
